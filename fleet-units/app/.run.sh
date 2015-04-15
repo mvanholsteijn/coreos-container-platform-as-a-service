3a34e884e80904a7629ddc438e75948692dc4968
Mark van Holsteijn <mark.van.holsteijn@gmail.com>
2015-04-15 15:49:22 +0200

    application with movable redis server and persistent data on a NAS

--- fleet-units/app/.run.sh
+++ fleet-units/app/.run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 fleetctl submit app-hellodb@.service
 fleetctl submit app-redis.service
+fleetctl submit mnt-data.service
+fleetctl load mnt-data.service
 fleetctl start app-redis.service
 fleetctl start app-hellodb@1.service
 fleetctl start app-hellodb@2.service
