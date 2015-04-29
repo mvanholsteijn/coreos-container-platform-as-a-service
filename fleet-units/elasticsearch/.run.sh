c5ad614b3c007e18d215920f943d8453a9415911
Mark van Holsteijn <mark.van.holsteijn@gmail.com>
2015-04-29 20:28:30 +0000

    rename

---
+++ fleet-units/elasticsearch/.run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo INFO: submitting fleet unit files
+for unit in mnt-data.mount elasticsearch@.service ; do
+	fleetctl submit $unit
+done
+
+echo INFO: loading fleet unit files
+for unit in mnt-data.mount elasticsearch@{1..3}.service ; do
+	fleetctl load $unit
+done
+echo INFO starting ElasticSearch cluster nodes..
+fleetctl start elasticsearch@${1..3}.service
