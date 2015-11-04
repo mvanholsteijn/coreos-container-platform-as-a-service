3e41a35903d75473b0368bf0f74800547234e81c
Mark van Holsteijn <mark.van.holsteijn@gmail.com>
2015-11-04 14:27:21 +0100

    cleaned up boot

--- vagrant/config.rb
+++ vagrant/config.rb
@@ -1,50 +1,35 @@
-$num_instances=3
+$num_instances=1
+$vm_memory=1024
+$vm_cpu=1
 $update_channel='stable'
 $expose_consul_tcp = true
 $forwarded_ports = { 80 => 8080 }
 
-$number_of_nfs_servers = 1
-
-#$datadog_api_key = '12325455'
+$number_of_nfs_servers = 0
 
 $new_discovery_url = 'https://discovery.etcd.io/new'
 
-# To automatically replace the discovery token on 'vagrant up'
-# and the data dog api key
-
+# delete the generated user_data on vagrant destroy
 if File.exists?('user_data') && ARGV[0].eql?('destroy')
   File.delete('user_data')
 end
 
+# automatically replace the discovery token on 'vagrant up' 
 if !File.exists?('user_data') && File.exists?('user_data.template') && ARGV[0].eql?('up')
   require 'open-uri'
   require 'yaml'
  
   token = open($new_discovery_url).read
  
-  data = YAML.load(IO.readlines('user_data.template')[1..-1].join)
-  data['coreos']['etcd']['discovery'] = token
-
-  if $datadog_api_key
-    updated = false
-    datadog_env = { 'path' => '/etc/datadog.env', 'permission' => '0400', 'owner' => 'root', 'content' => 'API_KEY=' + $datadog_api_key }
-    if data['write_files'] 
-        data['write_files'].each do |value| 
-                if value['path'] == '/etc/datadog.env'
-                        value['content'] = datadog_env['content']
-                        updated = true
-                end
-        end
-        if not updated
-           data['write_files'] = data['write_files'] + [ datadog_env ]
-        end
-    else
-       data['write_files'] = [ datadog_env ]
-    end
+  begin
+    data = YAML.load(IO.readlines('user_data.template')[1..-1].join)
+  rescue Exception => e
+    puts "ERROR: reading user_data.template"  + e.message
+    exit 
   end
+
+  data['coreos']['etcd']['discovery'] = token
 
   yaml = YAML.dump(data)
   File.open('user_data', 'w') { |file| file.write("#cloud-config\n\n#{yaml}") }
 end
-
-
